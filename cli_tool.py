03281ad8bbc4aedd2d86bfd94e8109ae878fc488
justintong25 <65700226+justintong25@users.noreply.github.com>
2024-04-30 18:40:27 +0800

    fixed overlapping graphs

--- cli_tool.py
+++ cli_tool.py
@@ -788,6 +788,7 @@ on how to use a command, type "help <command>"\n
 
         print("\n\nThe time difference list: \n\n", diff_times)
         print("\n\n")
+        plt.clf()
         plt.plot(diff_times, color='red+')
         plt.title("Packet Arrival Time Difference in Milliseconds")
         plt.xlabel("Packet #")
